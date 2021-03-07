369e8ff974e6a42ab7727d465cf9ae7cebaa4e6d
Jim Administrator <jamesdavidconner@gamil.com>
2021-03-07 08:05:21 -0600

    cleaned up index.html

---
+++ src/components/navBar.js
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Bio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Technology</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Projects</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</div>
+  <div class="p-5 text-center bg-light">
+      <h1 class="mb-3">Jim Conner</h1>
+      <h4 class="mb-3">My Coding Journey</h4>
+    </div>
+</nav>
---
+++ src/components/tech.js
@@ -0,0 +1,8 @@
+<h4>TECHNOLOGIES</h4>
+    <hr>
+   <ul>
+     <li>HTML</li>
+     <li>CSS</li>
+     <li>Javascript</li>
+     <li>Slack</li>
+   </ul>
